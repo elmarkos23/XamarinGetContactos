f0912780c141b8d2da0447dc58d5534775019fff
Markos Ayala <elmarkos23@gmail.com>
2019-02-07 15:57:36 -0500

    Initial commit

---
+++ XamarinGetContactos/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinGetContactos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            funCargar();
+        }
+        async void funCargar()
+        {
+
+            try
+            {
+
+                var contacts = await Plugin.ContactService.CrossContactService.Current.GetContactListAsync();
+                
+                lvContactos.ItemsSource = contacts;
+            }
+            catch (Exception ex)
+            {
+               await DisplayAlert("Error",ex.Message.ToString(),"OK");
+            }
+        }
+    }
+}
